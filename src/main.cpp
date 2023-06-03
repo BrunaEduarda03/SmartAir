093c9176956874099a88336f79e48b4b38517aec
Igor Alves <84601688+eee-gor@users.noreply.github.com>
2023-06-03 13:57:48 -0300

    Update main.cpp
    Agora funciona o envio de sinais infravermelho.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
 
 /*-------- Configurações ----------- */  
 #define DHTTYPE DHT11     // define o tipo do DHT
@@ -14,6 +16,19 @@
 #define RELAY_PIN 19      // ESP32 pin GIOP19
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
+/*-------- Variáveis ----------- */
+
+const uint16_t kIrLed = 19;     // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+IRsend irsend(kIrLed);          // Set the GPIO to be used to sending the message.
+// Código RAW (aumentar volume)
+uint16_t rawData[67] = {4542, 4506,  612, 1632,  602, 1644,  582, 1664,  578, 546,
+                        552, 572,  552, 572,  550, 574,  542, 582,  528, 1716,  
+                        550, 1694,  548, 1698,  534, 590,  524, 598,  530, 596,  
+                        516, 606,  530, 594,  520, 1724,  526, 1718,  534, 1710,  
+                        530, 596,  510, 614,  502, 620,  506, 616,  516, 608,  
+                        504, 618,  498, 626,  506, 618,  512, 1732,  516, 1728, 
+                        512, 1732, 524, 1720,  506, 1742,  510};
+
 
 /*-------- Token de Autenticação ----------- */
 char auth[] = "G2U5QNcQP7bSBMxRQa9pyhPybejZEaXS";
@@ -21,9 +36,8 @@ char auth[] = "G2U5QNcQP7bSBMxRQa9pyhPybejZEaXS";
 char ssid[] = "connect-izaias";
 char password[] = "244466666"; 
 
-/*-------- Handle das tasks ----------- */
-TaskHandle_t SensorPessoas;     // task handle da função que conta o numero de pessoas
-TaskHandle_t Sensor_DHT11;      // task handle da função de aquisição do DHT11
+TaskHandle_t Sensor_Task;       // task handle da função dos sensores
+
 float Temp;                     // variável para armazenar Temperatura em Celsius
 float Humidity;                 // variável para armazenar a humidade em %
 float HITemp;                   // variável para armazenar a sensação térmica em Celcius
@@ -45,9 +59,23 @@ void myTimer()
   Blynk.virtualWrite(V3,NumeroPessoas);
 }
 
+BLYNK_WRITE(V2) // Executes when the value of virtual pin 2 changes
+{
+  if(param.asInt() == 0)
+  {
+    Serial.println("NEC");
+    irsend.sendNEC(0xE0E0E01F); // Envia valor em HEX em protocolo NEC
+    delay(2000);
+    Serial.println("a rawData capture from IRrecvDumpV2");
+    irsend.sendRaw(rawData, 67, 38);  // Send a raw data capture at 38kHz.
+    delay(2000);
+  }
+}
+
 void setup() 
 {
   Serial.begin(115200);                         // Inicializa monitor serial
+  irsend.begin();                               // Inicializa o envio por infravermelho
   Blynk.begin(auth, ssid, password);            // Inicializa o Blynk
   timer.setInterval(1000L, myTimer);            // Inicializa o Timer do blynk para enviar os dados para o APP
   dht.begin();                                  // Inicializa o DHT111
@@ -57,36 +85,30 @@ void setup()
 
   pinMode(TCRT1_PIN, INPUT);    //definindo o pino do sensor como entrada
   pinMode(TCRT2_PIN, INPUT);    //definindo o pino do sensor como entrada
-  pinMode(RELAY_PIN, OUTPUT);   //Define o Pino como saída
-  digitalWrite(RELAY_PIN, LOW); //LED INICIA DESLIGADO
 
   xTaskCreatePinnedToCore(
-      SensorDHT11,        /* Function to implement the task */
-      "Task1",            /* Name of the task */
-      10000,              /* Stack size in words */
-      NULL,               /* Task input parameter */
-      0,                  /* Priority of the task */
-      &Sensor_DHT11,      /* Task handle. */
-      0);                 /* Core where the task should run */
-
-  xTaskCreatePinnedToCore(
-      SensorNumeroTask,   /* Function to implement the task */
+      SensoresTask,       /* Function to implement the task */
       "Task2",            /* Name of the task */
       10000,              /* Stack size in words */
       NULL,               /* Task input parameter */
       0,                  /* Priority of the task */
-      &SensorPessoas,     /* Task handle. */
+      &Sensor_Task,       /* Task handle. */
       0);                 /* Core where the task should run */
 
   delay(1000);
 }
 
-/*-------- Função para verificar entrada de pessoas na sala ----------- */ //(Utilizando LDR por enquanto)
-void SensorNumeroTask(void * parameter) 
+/*-------- Função para verificar entrada de pessoas na sala ----------- */
+void SensoresTask(void * parameter) 
 {
   for(;;) 
   {
-    // Verifica se sensor 1 está coberto
+    /*--------  captura valor da temperatura e Humidade ----------- */
+    Humidity = dht.readHumidity();                        // lê a humidade do ar
+    Temp = dht.readTemperature();                         // lê temperatura em °C
+    HITemp = dht.computeHeatIndex(Temp, Humidity, false); //calcula o indice de calor em celcius
+
+    /*-------- Verifica se sensor 1 está coberto ----------- */
     if(digitalRead(TCRT1_PIN) == 0)
     {
       while(digitalRead(TCRT1_PIN) == 0); // Entra em loop até sensor 1 ser descoberto
@@ -109,32 +131,6 @@ void SensorNumeroTask(void * parameter)
               NumeroPessoas--;     //Decrementa numero de pessoas
         }
     }
-  }
-}
-
-/*-------- Task para capturar valor da temperatura e Humidade ----------- */
-void SensorDHT11(void * parameter)
-{
-  for(;;) 
-  {
-    Humidity = dht.readHumidity();                        // lê a humidade do ar
-    Temp = dht.readTemperature();                         // lê temperatura em °C
-    HITemp = dht.computeHeatIndex(Temp, Humidity, false); //calcula o indice de calor em celcius
-    delay(100);
-  }
-}
-
-BLYNK_WRITE(V2) // Executes when the value of virtual pin 2 changes
-{
-  if(param.asInt() == 1)
-  {
-    // executa o codigo se o widget do botão no APP é pressionado (ON) 
-    digitalWrite(RELAY_PIN,HIGH);  // Set digital pin 19 HIGH
-  }
-  else
-  {
-    // executa o codigo se o widget do botão no APP é desligado (OFF) 
-    digitalWrite(RELAY_PIN,LOW);  // Set digital pin 19 LOW 
   }
 }
 
